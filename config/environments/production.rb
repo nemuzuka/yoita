bc4b65cb1671c36db6e9bfa562d633b38f37cadf
nemuzuka <kkaz123@gmail.com>
2012-07-30 23:39:41 +0900

    静的ファイルを事前にコンパイルしなくても動作するように設定変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 Yoita::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
@@ -15,7 +17,8 @@ Yoita::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  # TODO とりあえずtrue
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
